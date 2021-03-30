c8c5bf1132deb4e0bfa824d58fc7ed4a2720005a
Andrey Vdovinov <andrey.vdovinov@gmail.com>
2021-03-30 22:22:58 +0300

    Hello

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 class Hello
 {
-	public static void main ( String[] args)
+	public static void main ( String[] args )
 	{
-		System.out.println("Hello world!");
+		System.out.println( "Hello world!" );
 	}
 }
